c9086c97cfba6726b2d7c0978fa171fbe2ca1c97
edwinogwel <edogwel@gmail.com>
2022-03-10 07:48:55 -0800

    add task 1-5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints "Programming is like building a multilingual puzzle
+ * Return 0: if exited properly else any other value
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main() - prints - with proper grammar, but the outcome is a piece of art,
+ * Return: 0 if exited properly else any other value
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
