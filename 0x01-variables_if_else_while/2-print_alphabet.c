e23dfc289cda9a5b19e83603bda22294bf1bccd9
edwinogwel <edogwel@gmail.com>
2022-03-11 05:23:27 -0800

    fix bugs

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,6 +12,6 @@ int main(void)
 		putchar(c);
 		c++;
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,5 +9,6 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 		printf("%d", i);
+	printf("\n");
 	return (0);
 }
