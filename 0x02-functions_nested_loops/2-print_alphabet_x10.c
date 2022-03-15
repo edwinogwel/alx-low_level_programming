9168a079d1efd9693020a3e329c8326636f095c1
edwinogwel <edogwel@gmail.com>
2022-03-15 12:43:14 +0000

    add task 1 & 2 and README file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ * Return: 0 sucess
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
