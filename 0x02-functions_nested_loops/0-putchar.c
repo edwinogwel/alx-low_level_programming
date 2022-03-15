6ece76307a1fb47730277bd28acda547f496b08f
edwinogwel <edogwel@gmail.com>
2022-03-15 13:15:12 +0000

    add task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
