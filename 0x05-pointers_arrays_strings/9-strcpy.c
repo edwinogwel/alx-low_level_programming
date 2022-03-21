809c7c086d7225a1bd6b7b93391e6b4382158f99
edwinogwel <edogwel@gmail.com>
2022-03-21 20:39:23 +0000

    fix: a typo

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,13 @@
  * *_strcpy - copies the string pointed to by src, including the
  * terminating null byte(\0), to the buffer pointed by dest
  * @dest: pointer to the buffer where the str is copied to
- * @src: pointer to str to be copied
+ * @src: pointer to the str to be copied
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
+	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
 		dest[i] = src[i];
 
