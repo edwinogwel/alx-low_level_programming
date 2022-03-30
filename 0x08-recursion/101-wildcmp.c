f8e8d2c11bbdc1232e0b5514dc95b3737f8109f0
edwinogwel <64576329+edwinogwel@users.noreply.github.com>
2022-03-31 02:13:58 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -56,6 +56,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		s2 = move(s2);
+                if (*s2 && *s1 == *s2)
+                {
+                        break;
+                        return (1);
+                }
 		if (*s2 == '\0')
 			return (1);
 		if (*s1 == *s2)
