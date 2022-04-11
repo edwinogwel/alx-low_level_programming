a3799324004f8fcc71be5d382e02403bf3b9fc4f
edwinogwel <edogwel@gmail.com>
2022-04-11 15:18:21 +0000

    add: task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure definition of a dog
+ * @name: char str
+ * @age: int
+ * @owner: char str
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
