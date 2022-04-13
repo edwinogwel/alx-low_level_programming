d928f054a9048412f8f3cff46350dc38a664b252
edwinogwel <edogwel@gmail.com>
2022-04-13 19:42:31 +0000

    include: stdlib

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNC_PTR_H
 #define FUNC_PTR_H
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
